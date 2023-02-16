17523b21d0e8ef80c5c2e36a1c9e3cbee805d8eb
aaweaver-actuary <53792857+aaweaver-actuary@users.noreply.github.com>
2023-02-16 11:16:31 -0500

    lab4 notebook

---
+++ S02 - Spring 2023/Labs/lab4.R
@@ -0,0 +1,18 @@
+# Hang the spring (narrow end up) from the ring stand. Hang
+# a weight holder from the other end of the spring and place
+# the range finder on the ground underneath the weight holder.
+# You will take data by connecting the range finder to a USB
+# interface box and using the Data Studio software. Set the
+# experiment up so that you get a three column list where
+# the first column is the time data, the second column is the
+# position data, the third column is the velocity data. Save
+# your data as a "CSV" ("Comma Separated Values") file.  
+
+# Use a plumb to align the weight holder and the range finder.
+# In order to get good data, the range finder must always be
+# a least 0.5 meters from the weight holder. Put several
+# different weights on the mass holder and use the range
+# finder to measure the period of oscillation for several
+# different masses. 
+
+Since
